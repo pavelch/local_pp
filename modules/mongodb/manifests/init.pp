8246ddcf391e9c5af578cebb6b5fd851d4662371
Pavel Chernovsky <pavelch@outlook.com>
2015-05-03 14:27:38 +0300

    mongodb use latest stable from 3.0

--- modules/mongodb/manifests/init.pp
+++ modules/mongodb/manifests/init.pp
@@ -5,9 +5,10 @@ class mongodb {
     $db_dirs = [ "/data/", "/data/configdb"]
 
     file { "/etc/yum.repos.d/mongodb.repo":
-        content => "[mongodb]
+        content => "
+[mongodb-org-3.0]
 name=MongoDB Repository
-baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64
+baseurl=http://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.0/x86_64/
 gpgcheck=0
 enabled=1
 ",
