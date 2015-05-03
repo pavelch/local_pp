93fe0031fdf60a5c3f0ac65bf50f6b337975301a
Pavel Chernovsky <pavelch@outlook.com>
2015-05-03 17:24:55 +0300

    dot in path

--- modules/profile/manifests/basic.pp
+++ modules/profile/manifests/basic.pp
@@ -8,7 +8,7 @@ class profile::basic {
   file { ".aws":
     ensure => directory
   }->
-  exec { "cp /vagrant/.aws/credentials /root/.aws/": }->
+  exec { "cp /vagrant/\.aws/credentials /root/\.aws/credentials": }->
   exec { "dos2unix /root/.aws/credentials": }
   package { 'aws-sdk':
     ensure   => 'installed',
