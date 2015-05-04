8c4755e3c56453c95e3ab3bc68f892b8152f462e
Pavel Chernovsky <pavelch@outlook.com>
2015-05-04 17:05:55 +0300

    maven module in progress

--- modules/profile/manifests/maven.pp
+++ modules/profile/manifests/maven.pp
@@ -1,5 +1,17 @@
 class profile::maven {
-  class { "maven::maven":
-    version => "3.3.3"
+  exec { "wget http://apache.mivzakim.net/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz": }->
+  exec { "mv apache-maven-3.3.3 /opt/": }->
+  file { '/usr/bin/mvn':
+    ensure => link,
+    target => "/opt/apache-maven-3.3.3/bin/mvn"
+  }->
+  file { "/etc/profile.d/maven.sh":
+    content => "
+#!/bin/bash
+MAVEN_HOME=/opt/maven
+PATH=$MAVEN_HOME/bin:$PATH
+export PATH MAVEN_HOME
+export CLASSPATH=.
+    "
   }
 }
