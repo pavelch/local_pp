1dd3330db299e8d44a98798e9aa3af67751d95f6
Pavel <pavelch@outlook.com>
2015-10-11 17:59:32 +0300

    typo

--- modules/profile/manifests/maven.pp
+++ modules/profile/manifests/maven.pp
@@ -2,7 +2,7 @@ class profile::maven {
   include profile
 
   exec { "curl -O http://apache.mivzakim.net/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz": }->
-  exec { "tar xvf apache-maven-3.3.3-bin.tar.gz" }->
+  exec { "tar xvf apache-maven-3.3.3-bin.tar.gz": }->
   exec { "mv apache-maven-3.3.3 /opt/": }->
   file { '/usr/bin/mvn':
     ensure => link,
